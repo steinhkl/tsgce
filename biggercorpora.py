60cbff2ee90083c1cb0a8389cf25d25ebf57111b
steinhkl <klaus@steinhkl.de>
2017-12-05 15:20:39 +0100

    add bigger corpora and modify start.py accordingly

---
+++ biggercorpora.py
@@ -0,0 +1,32 @@
+
+# for manual google querys and parsing of resulting htmls
+import requests
+from bs4 import BeautifulSoup
+
+def parsegooglehtml(searchstring):
+    r = requests.get("https://www.google.com/search", params={'q':searchstring})
+    soup = BeautifulSoup(r.text, "lxml")
+    res = soup.find("div", {"id": "resultStats"})
+    return int(res.text.replace(".", "").split()[1])
+    pass
+
+
+def main(listofsentences):
+    # create a dictionary for the results:
+    parsehtmlresults = {}
+
+    # here we work on our sentences
+    for sentence in listofsentences:
+        # send all senteces to google and parse resulting HTML file
+        parsehtmlresults[sentence] = parsegooglehtml(sentence)
+        pass
+
+    # Order by Number of results.
+    parsehtmlresults = sorted(parsehtmlresults.items(), key=lambda x:x[1], reverse=True)
+
+    # return sorted key:value pairs
+    return parsehtmlresults
+
+
+if __name__ == "__main__":
+        main()
--- start.py
+++ start.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python3
 import os
 import sys
 import ast
+from collections import OrderedDict
+
 
 import check_checkedCorpera
 import tagandroll
+import biggercorpora
 
 def main():
 
@@ -12,15 +16,21 @@ def main():
     else:
         print("Please give me the sentence you want to get checked:")
         userInput = input()
-    
+
     result1 = check_checkedCorpera.main(userInput)
 
     if result1 == 1:
         listofsentences = tagandroll.main(userInput)
-        
-        # THIS IS WHAT NEXT GROUP USES
-        print(listofsentences)
 
-        
+        # start group 3
+        # remove duplicates but keep order
+        # source https://stackoverflow.com/questions/7961363/removing-duplicates-in-lists
+        listwithoutduplicates = OrderedDict((x, True) for x in listofsentences).keys()
+        # call group3 python program
+        ranking = biggercorpora.main(listwithoutduplicates)
+        print(ranking)
+
+
+
 if __name__ == "__main__":
-        main()
+    main()
