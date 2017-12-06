7b8eb1c23b4c7e52382cd2d8cc20914babac62bb
steinhkl <klaus@steinhkl.de>
2017-12-06 10:24:59 +0100

    fix error when there is only one result

--- biggercorpora.py
+++ biggercorpora.py
@@ -4,17 +4,21 @@ import requests
 from bs4 import BeautifulSoup
 
 def parsegooglehtml(searchstring):
+    searchstring = '"'+searchstring+'"'
     r = requests.get("https://www.google.com/search", params={'q':searchstring,'nfpr':1})
     soup = BeautifulSoup(r.text, "lxml")
     res = soup.find("div", {"id": "resultStats"})
-    return int(res.text.replace(".", "").split()[1])
+    try:
+        result = int(res.text.replace(".", "").split()[1])
+    except:
+        result = 1
+    return result
     pass
 
 
 def main(listofsentences):
     # create a dictionary for the results:
     parsehtmlresults = {}
-
     # here we work on our sentences
     for sentence in listofsentences:
         # send all senteces to google and parse resulting HTML file
