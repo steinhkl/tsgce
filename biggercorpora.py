9beeaad1a92edc1855b6b4ae29d7944e8d9e460e
steinhkl <klaus@steinhkl.de>
2017-12-07 14:14:32 +0100

    refactoring

--- biggercorpora.py
+++ biggercorpora.py
@@ -5,50 +5,50 @@ from bs4 import BeautifulSoup
 import pprint
 from googleapiclient.discovery import build
 
-def parsegooglehtml(searchstring):
-    #searchstring = '"'+searchstring+'"'
-    r = requests.get("https://www.google.com/search", params={'q':searchstring,'nfpr':1})
-    # check for rate limit
+def GoogleAPILimit(APIKey):
+    # returns true if API Key Call Limit has not been reached yet.
+    # TODO: check API Call Limit for Key.
+    return True
+    pass
+
+def GoogleParseLimit():
+    # returns true if you can still query google.
+    r = requests.get("https://www.google.com/search", params={'q':'test my query limit','nfpr':1})
     if r.status_code == 503:
-        print("You have reached the WEB rate limit. Please wait.")
-        return -1
+        return False
+    else:
+         return True
+    pass
+
+def parsegooglehtml(searchstring):
+    r = requests.get("https://www.google.com/search", params={'q':searchstring,'nfpr':1})
+    # parses HTML output.
+    soup = BeautifulSoup(r.text, "lxml")
+
+    # TODO: This probably does not work outside of germany
+    if soup.find(text="Keine Ergebnisse für "):
+        return 0
         pass
 
-    else:
-        # parse HTML output.
-        soup = BeautifulSoup(r.text, "lxml")
-
-        # Don't be fooled by suggested results.
-        # TODO: This probably does not work outside of germany
-        # edit to text="No resulst for " if google returns unlikely numbers.
-        if soup.find(text="Keine Ergebnisse für "):
-            print("No results found")
-            return 0
-            pass
-
-        # find the div which contains number of results
-        res = soup.find("div", {"id": "resultStats"})
-        try:
-            # if there is more than 1 result the query will return:
-            # "About XXXX results"
-            result = int(res.text.replace(".", "").split()[1])
-        except:
-            # otherwise it will return "1 Result"
-            result = int(res.text.replace(".", "").split()[0])
-        return result
+    # find the div which contains number of results
+    res = soup.find("div", {"id": "resultStats"})
+    try:
+        # if there is more than 1 result the query will return: "About XXXX results"
+        result = int(res.text.replace(".", "").split()[1])
+    except:
+        # otherwise it will return "1 Result"
+        result = int(res.text.replace(".", "").split()[0])
+    return result
 
     pass
 
-def queryGoogleCSEApi(searchstring):
-    # we read devkey from file so it is not in github.
-    devKey = open("google-api-key.txt").read().strip()
+def queryGoogleCSEApi(searchstring, devKey):
     # build API Service
     service = build("customsearch", "v1", developerKey=devKey)
     # Specify Custom Search Engine.
     cseKey = "014193703923044144161:os4pqh4une0"
     res = service.cse().list(q=searchstring,cx=cseKey,).execute()
     return (res.get('searchInformation').get('totalResults'))
-
     pass
 
 
@@ -56,13 +56,23 @@ def main(listofsentences):
     # create a dictionary for the results:
     parsehtmlresults = {}
     queryGoogleAPIresults = {}
+    # we read devkey from file so it is not in github.
+    devKey = open("google-api-key.txt").read().strip()
+    # Check wether we have reached API Calllimit.
+    APILimit = GoogleAPILimit(devKey)
+    HTMLLimit = GoogleParseLimit()
+
     # here we work on our sentences
     for sentence in listofsentences:
         # make it quoted for exact matching search results.
         sentence = '"'+sentence+'"'
         # send all senteces to google and parse resulting HTML file
-        parsehtmlresults[sentence] = parsegooglehtml(sentence)
-        queryGoogleAPIresults[sentence] = queryGoogleCSEApi(sentence)
+        if HTMLLimit:
+            parsehtmlresults[sentence] = parsegooglehtml(sentence)
+            pass
+        if APILimit:
+            queryGoogleAPIresults[sentence] = queryGoogleCSEApi(sentence, devKey)
+            pass
         pass
 
     # Order by Number of results.
