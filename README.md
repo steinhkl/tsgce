72825312486ffe4340291ffb37948cd8b265f517
steinhkl <klaus@steinhkl.de>
2017-12-07 09:45:45 +0100

    added Google CSE API to the bigger corpora search. Works but doesnt normaly display it yet. Waiting for data structure

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # tsgce
 Tiny Statistical Grammar Checking Engine
 
-
-## Dependencies
-### NLTK
+## Installing
+### Dependencies
+#### NLTK
 [NLTK](http://www.nltk.org/)
 
 ``` bash
-pip install nltk
+pip3 install nltk
 ```
 
-### Pattern
+#### Pattern
 
 This Programm depends on the development branch of the [pattern](https://github.com/clips/pattern/) module. To install it, use following commands:
 
@@ -24,12 +24,26 @@ python setup.py install
 
 It is possible, that and ModuleNotFoundError pops while installing pattern. So if "ModuleNotFoundError: No module named 'numpy'" pops up, simply install it by running pip:
 ``` bash
-pip install numpy
+pip3 install numpy
 ```
 
-Other dependencies that may need to be installed:
+#### Other dependencies
 
 ``` bash
 pip3 install bs4
 pip3 install lxml
+pip3 install google-api-python-client
 ```
+
+## Google API key
+
+This Program uses the Google API and requires an API Key. You can create a Project and add an API Key at   https://console.developers.google.com/apis/
+
+Put the key in google-api-key.txt .
+
+## Google CSE
+
+I am not sure if you need to create your own CSE but I don't think so.  
+However: You can create a Custom Search Engine at https://cse.google.com/   
+After adding it make sure to select "Search the entire web but emphasize included sites" for the Option "Sites to search"  
+You can then modify biggercorpora.py  line 48.
--- biggercorpora.py
+++ biggercorpora.py
@@ -2,9 +2,11 @@
 # for manual google querys and parsing of resulting htmls
 import requests
 from bs4 import BeautifulSoup
+import pprint
+from googleapiclient.discovery import build
 
 def parsegooglehtml(searchstring):
-    searchstring = '"'+searchstring+'"'
+    #searchstring = '"'+searchstring+'"'
     r = requests.get("https://www.google.com/search", params={'q':searchstring,'nfpr':1})
     # check for rate limit
     if r.status_code == 503:
@@ -37,18 +39,39 @@ def parsegooglehtml(searchstring):
 
     pass
 
+def queryGoogleCSEApi(searchstring):
+    # we read devkey from file so it is not in github.
+    devKey = open("google-api-key.txt").read().strip()
+    # build API Service
+    service = build("customsearch", "v1", developerKey=devKey)
+    # Specify Custom Search Engine. 
+    cseKey = "014193703923044144161:os4pqh4une0"
+    res = service.cse().list(q=searchstring,cx=cseKey,).execute()
+    return (res.get('searchInformation').get('totalResults'))
+
+    pass
+
 
 def main(listofsentences):
     # create a dictionary for the results:
     parsehtmlresults = {}
+    queryGoogleAPIresults = {}
     # here we work on our sentences
     for sentence in listofsentences:
+        # make it quoted for exact matching search results.
+        sentence = '"'+sentence+'"'
         # send all senteces to google and parse resulting HTML file
         parsehtmlresults[sentence] = parsegooglehtml(sentence)
+        queryGoogleAPIresults[sentence] = queryGoogleCSEApi(sentence)
         pass
 
     # Order by Number of results.
     parsehtmlresults = sorted(parsehtmlresults.items(), key=lambda x:x[1], reverse=True)
+    #TODO: This sorting does not seem to work yet. But we probably wont need it anymore once we have a data structure.
+    queryGoogleAPIresults = sorted(queryGoogleAPIresults.items(), key=lambda x:x[1], reverse=True)
+
+    # TODO: Add Results to JSON Data.
+    # THIS IS WHERE I WOULD PUT MY JSON IF I HAD ANY!
 
     # return sorted key:value pairs
     return parsehtmlresults
