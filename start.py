c5a72668ca0db963b82a7655ab307ec62b408f59
Tobias Michalski <tobias.michalski@bsk-consulting.de>
2017-12-20 10:31:18 +0100

    Refactored TagAndRoll so that it handles json internal. Modified start.py so that 3rd groups code still starts properly with new tagandroll.

--- start.py
+++ start.py
@@ -18,15 +18,18 @@ def main():
     result1 = check_checkedCorpera.main(userInput)
 
     if result1 == 1:
-        listofsentences = tagandroll.main(userInput)
+        jsong2 = tagandroll.main(userInput)
+        listofsentences = []
+        for sentence in jsong2["forms"]:
+            listofsentences.append(sentence["sentence"])
 
         # start group 3
         # remove duplicates but keep order
         # source https://stackoverflow.com/questions/7961363/removing-duplicates-in-lists
         listwithoutduplicates = OrderedDict((x, True) for x in listofsentences).keys()
         # call group3 python program
-        ranking = biggercorpora.main(listwithoutduplicates)
-        print(ranking)
+        listofsentences = biggercorpora.main(listwithoutduplicates)
+        print(listofsentences)
 
 
 
--- tagandroll.py
+++ tagandroll.py
@@ -10,6 +10,16 @@ formaliases = ["inf", "1sg", "2sg", "3sg", "pl", "part", "1sgp", "2sgp", "3sgp",
 # This comes from First group in some way
 inputtext = "The dog walks away."
 
+def retaglist (jsonlist):
+    
+    for idx, form in enumerate(jsonlist["forms"]):
+        #Tag Text
+        taggedtext = nltk.word_tokenize(form["sentence"])
+        taggedtext = nltk.pos_tag(taggedtext)
+        jsonlist["forms"][idx]["tagged"] = taggedtext
+
+    return jsonlist
+
 # Create sentance given tagged text, the word to be changed and the index of it
 def createsentence(sentence, wordformed, index):
     out = ""
@@ -32,54 +42,58 @@ def createsentence(sentence, wordformed, index):
     return out
 
 # Function to create a list of sentences with changed verbs
-def inflictverbs(sentencestocheck):
+def inflictverbs(jsonsentence):
     outlist = []
     verbfound = False
-    #For all sentences to change
-    for sentence in sentencestocheck:
-        i = 0
-
-        # Check every tag of sentence
-        for word in sentencestocheck[sentence]:
-            # Look for Verbs
-            if word[1][0] == 'V':
-                verbfound = True
-                for alias in formaliases:
-                    outlist.append(createsentence(sentencestocheck[sentence], conjugate(word[0], alias), i))
-            i = i + 1
+    ##For all sentences to change
+    #for sentence in sentencestocheck:
+    #    i = 0
+    i = 0
+    # Check every tag of sentence
+    for word in jsonsentence["tagged"]:
+        # Look for Verbs
+        if word[1][0] == 'V':
+            verbfound = True
+            for alias in formaliases:
+                sentenceformed = { "sentence" : createsentence(jsonsentence["tagged"], conjugate(word[0], alias), i)}
+                outlist.append(sentenceformed)
+        i += 1
     if verbfound == False:
         print("No verb found, returning empty list")
 
     return outlist
 
 # This will generate this list of sentences (curently only changed verbs. Will do other stuff later)
-def genlist(sentencestocheck):
-    listofsentences = {}
-    listofsentences = inflictverbs(sentencestocheck)
+def genlist(jsonsentence):
+    listofsentences = []
+    jsonsentence = inflictverbs(jsonsentence) 
 
-
-    return listofsentences
+    return jsonsentence
 
 def main(intext):
     #Check for missing resources
+    jsonout = {}
+    jsonout["sentence"] = intext
     try:
         nltk.data.find("taggers/averaged_perceptron_tagger")
     except:
         print("Loading missing libraries.")
         nltk.download("averaged_perceptron_tagger")
 
-
+    
     #Tag input Text
     taggedtext = nltk.word_tokenize(intext)
     taggedtext = nltk.pos_tag(taggedtext)
+    jsonout["tagged"] = taggedtext
 
     # Generate variations of sentence
-    sentencestocheck = {intext : taggedtext}
-    sentenceforms = genlist(sentencestocheck)
+
+    jsonout["forms"] = genlist(jsonout)
 
     # This goes to next group
-    return sentenceforms
+    #print(jsonout)
+    return jsonout
 
 
 if __name__ == "__main__":
-        main(inputtext)
+    main(inputtext)
